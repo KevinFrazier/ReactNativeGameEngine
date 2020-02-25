cc0cb00a4434a65570e1333f44d2003b8bed5c87
KevinFrazier <kevifraz@gmail.com>
2020-02-24 18:04:36 -0800

    finished joystick movement, bug: when another action is committed (press), it will not set Velocity of world'

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,198 +1,168 @@
-import * as WebBrowser from 'expo-web-browser';
+
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Dimensions, StyleSheet, Text, View, StatusBar } from 'react-native';
+import Matter from "matter-js";
+import { GameEngine } from "react-native-game-engine";
+import Box from '../components/Box';
 
-import { MonoText } from '../components/StyledText';
+const { width, height } = Dimensions.get("screen");
+const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+const engine = Matter.Engine.create({ enableSleeping: false });
+const world = engine.world;
+const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
+const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
+Matter.World.add(world, [initialBox, floor]);
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+//for movePlayer()
+var moving = false;
+var joyStickRef = false;
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
+const Physics = (entities, { time }) => {
+  let engine = entities["physics"].engine;
+  Matter.Engine.update(engine, time.delta);
+  return entities;
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
+let boxIds = 0;
+
+const addObject = (entities, body) => {
+  let world = entities["physics"].world;
+
+  //add to world
+  Matter.World.add(world, [body]);
+
+  //add to entities
+  entities[++boxIds] = {
+    body: body,
+    size: [boxSize, boxSize],
+    color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+    renderer: Box
+  };
+
+}
+const CreateBox = (entities, { touches, screen }) => {
+  let world = entities["physics"].world;
+  world.gravity.scaling = 0
+  world.gravity.x = 0
+  world.gravity.y = 0
+
+  let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+  touches.filter(t => t.type === "press").forEach(t => {
+    let body = Matter.Bodies.rectangle(
+      t.event.pageX,
+      t.event.pageY,
+      boxSize,
+      boxSize,
+      {
+        frictionAir: 0.021,
+        restitution: 4.0
+      }
     );
 
+    addObject(entities,body)
+
+  });
+  return entities;
+};
+
+const movePlayer = (entities, {touches}) => {
+  let world = entities["physics"].world;
+
+  touches.filter(t => t.type === "end").forEach(t =>{
+
+    moving = false
+    Matter.Body.setVelocity(initialBox, {x: 0, y:0})
+    joyStickRef = {
+      x: 0,
+      y: 0,
+    }
+
+    
+  })
+  touches.filter(t => t.type === "move").forEach(t => {
+    
+    // console.log("t:")
+    // console.log(t)
+
+    //check off screen drag touches
+    const PROPAGATION = 5
+    if(t.event.pageX >= width - PROPAGATION || t.event.pageX <= 0 + PROPAGATION){
+      moving = false
+      Matter.Body.setVelocity(initialBox, {x: 0, y: 0})
+      joyStickRef = {
+        x: 0,
+        y: 0,
+      }
+    }
+    else if(t.event.pageY >= height - PROPAGATION || t.event.pageY <= 0 + PROPAGATION){
+      moving = false
+      Matter.Body.setVelocity(initialBox, {x: 0, y: 0})
+      joyStickRef = {
+        x: 0,
+        y: 0,
+      }
+    }
+    else{
+      
+      if(!moving){
+        //set moving
+        moving = true
+        //get joystick frame of reference
+        joyStickRef = {
+          x: t.event.pageX,
+          y: t.event.pageY,
+        }
+      }
+
+      console.log("moving")
+
+      //determine the direction of movement at point in time
+      const newPosition = {
+        x : t.event.pageX - joyStickRef.x,
+        y : t.event.pageY - joyStickRef.y
+      }
+
+      //determine the speed
+      const SPEED_COEFF = 10   
+
+      //calculate the unit vector
+      var denom = Math.sqrt(Math.pow(newPosition.x,2) + Math.pow(newPosition.y, 2))
+
+      //set Velocity
+      Matter.Body.setVelocity(initialBox, {x: SPEED_COEFF*newPosition.x/denom, y: SPEED_COEFF*newPosition.y/denom})
+
+
+    }
+
+  
+  });
+
+  return entities
+}
+
+
+export default class App extends React.Component {
+  render() {
     return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
+      <GameEngine
+        style={styles.container}
+        systems={[Physics, CreateBox, movePlayer]} // Array of Systems
+        entities={{
+          physics: { engine: engine, world: world },
+          floor: { body: floor, size: [width, boxSize], color: "green", renderer: Box },
+          initialBox: { body: initialBox, size: [boxSize, boxSize], color: 'red', renderer: Box }
+        }}
+      >
+        <StatusBar hidden={true} />
+      </GameEngine>
     );
   }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
